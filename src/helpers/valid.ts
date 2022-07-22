24fd83ed5a9431e08069e1f823827c14acda1744
User <belpc2012@gmail.com>
2022-07-23 00:55:33 +0300

    fix: validation login

--- src/helpers/valid.ts
+++ src/helpers/valid.ts
@@ -3,14 +3,14 @@ import * as Joi from '@hapi/joi';
 export const PutSchema = Joi.object({
     id: Joi.required(),
     login: Joi.string().required(),
-    password: Joi.string(),//.pattern(new RegExp('^[a-zA-Z0-9]{2,}$')),
+    password: Joi.string().pattern(new RegExp('^[A-Za-z0-9]{2,}$')),
     age: Joi.number().min(4).max(130).required(),
     isDeleted: Joi.boolean().required()
 });
 
 export const PostSchema = Joi.object({
     login: Joi.string().required(),
-    password: Joi.string(),//.pattern(new RegExp('^[a-zA-Z0-9]]{2,}$')),
+        password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{2,}$')),
     age: Joi.number().min(4).max(130).required(),
     isDeleted: Joi.boolean().required()
 });
