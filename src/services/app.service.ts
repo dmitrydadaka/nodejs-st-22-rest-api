a9c6333ab1eb087103e5d677db49b40f365c8457
User <belpc2012@gmail.com>
2022-08-02 14:44:00 +0300

    feat: refactored sequelize repository

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,6 +1,4 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { User } from '../interfaces/user.interface';
-import { v4 as uuidv4 } from 'uuid';
 import { UserEntity } from '../user.entity';
 import { PostSchema, PutSchema } from '../helpers/valid';
 import { SequelizeRepository } from '../repository/sequelize-repository';
@@ -8,31 +6,25 @@ import { SequelizeRepository } from '../repository/sequelize-repository';
 @Injectable()
 export class AppService {
   constructor(@Inject('User_REPOSITORY')
-  private usersRepository: typeof UserEntity,
   private sequelizeRepository: SequelizeRepository) { }
 
   public async getUsers(limit, loginSubstring): Promise<UserEntity[]> {
-    return await this.usersRepository.findAll()
-    .then((users) => this.sequelizeRepository.getAutoSuggestUsers(users, limit, loginSubstring));
+    return await this.sequelizeRepository.getAutoSuggestUsers(limit, loginSubstring)
   }
 
   public async findOne(id: string): Promise<UserEntity> {
-    return this.usersRepository.findOne<UserEntity>({ where: { id } });
+    return this.sequelizeRepository.findOne(id);
   }
 
   public async create(user: typeof PostSchema): Promise<UserEntity> {
-    return await this.usersRepository.create<UserEntity>({ id: uuidv4(), ...user });
+    return await this.sequelizeRepository.create(user);
   }
 
   public async remove(id: string): Promise<number> {
-    return await UserEntity.destroy({
-      where: {
-        id: id
-      }
-    });
+    return await this.sequelizeRepository.remove(id);
   }
 
   public async update(id: string, user: typeof PutSchema) {
-    return await this.usersRepository.update({ id, ...user }, { where: { id } });
+    return await this.sequelizeRepository.update(id, user);
   }  
 }
